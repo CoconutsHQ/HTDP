63539e900972bb84d34d83cb9efe456d3eb3b8b5
Akash Arun <akasharun2010@gmail.com>
2015-10-25 19:33:11 +0530

    Exercise 13 Done

--- akasharun/013/013.rkt
+++ akasharun/013/013.rkt
@@ -1,10 +1,8 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |013|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 #lang racket
 
-(define (result x y)
+(define (distance-point x y)
   (sqrt (+ (sqr x) (sqr y))))
+(define result distance-point)
 
 (provide result)
 
