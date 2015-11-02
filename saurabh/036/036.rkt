f16a759952d25da10320a5d03b76c0e8ec295437
Saurabh <saurabhsau7@gmail.com>
2015-11-02 19:54:28 +0530

    exercise 36- done

---
+++ saurabh/036/036.rkt
@@ -0,0 +1,11 @@
+#lang racket
+; string->string
+
+; extracts first character from the given string
+
+(check-expect (string-first "saurabh" ) "s" )
+(check-expect (string-first "" ) "")
+
+; (define (string-first str) (... s ...)
+
+(define (string-first str) (substring str 0 1))
