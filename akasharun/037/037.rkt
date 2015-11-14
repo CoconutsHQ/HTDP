2b40ab1c74145cbbef05c98567ea447d7164fe33
Akash Arun <akasharun2010@gmail.com>
2015-11-14 19:30:10 +0530

    Exercise 37 Done

---
+++ akasharun/037/037.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+; String -> String
+; Shows the last character of string
+; (string-first "thenga") => "a"
+; (string-first "kola") => "a"
+
+; Template
+; (define (string-first string)
+; (...string...))
+
+(define (string-last string)
+  (string-ith string (- (string-length string) 1)))
+
+(define result (list string-last))
+(provide result)
