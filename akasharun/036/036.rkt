094160ad26450487c2fb421508a5a1cdf9182282
Akash Arun <akasharun2010@gmail.com>
2015-11-14 19:19:36 +0530

    Edited

--- akasharun/036/036.rkt
+++ akasharun/036/036.rkt
@@ -1,10 +1,12 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |036|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#lang racket
+
 ; String -> String
 ; Shows the first character of string
-; (string-first "thenga") => "t"
-; (string-first "kola") => "k"
+; (string-first "thenga") => "a"
+; (string-first "kola") => "a"
 
 ; Template
 ; (define (string-first string)
