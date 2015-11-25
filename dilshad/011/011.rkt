3d93007c29f13c4aa6fce5efbfc01a692468633c
Dilshad <dilshadce@gmail.com>
2015-11-25 20:33:44 +0530

    011 added

---
+++ dilshad/011/011.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(require 2htdp/image)
+(define in "hello")
+(define result
+  (list
+   (if (string? in) (string-length in)
+      (if (image? in) (* (image-height in) (image-width in))
+        (if (number? in)
+            (if (<= in 0) in (- in 1))
+               (if (boolean=? in #true) 10 20))))))
+
+(provide result)
